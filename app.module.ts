1563acae40fb30ecefa897cda58a7d8c922992b4
JGaratL <120282132+JGaratL@users.noreply.github.com>
2023-05-04 00:21:59 +0200

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
