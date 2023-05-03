138858a0b548acf496c3fbc98b486ad2476e2a9b
JGaratL <120282132+JGaratL@users.noreply.github.com>
2023-05-04 01:30:47 +0200

    Update form.component.ts

--- form.component.ts
+++ form.component.ts
@@ -12,7 +12,7 @@ export class FormComponent {
     firstName: new FormControl('', [Validators.required]),
     lastName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phoneNumber: new FormControl(null, [Validators.required]),
+    phoneNumber: new FormControl(null, [Validators.required, Validators.pattern("^[0-9]*$")]),
     nationality: new FormControl('', [Validators.required]),
     userName: new FormControl('', [Validators.required, Validators.minLength(8)]),
     password: new FormControl('', [Validators.required, Validators.pattern( /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/)]),
